e59b466b34a67eb582b9126b7d755696e57c79c2
EKthor <richardfc83@gmail.com>
2024-04-25 18:40:14 -0500

    add images

--- LibraryBackend/Controllers/BooksController.cs
+++ LibraryBackend/Controllers/BooksController.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using LibraryBackend;
+﻿using Microsoft.AspNetCore.Mvc;
 using LibraryBackend.Models;
 using Microsoft.AspNetCore.Authorization;
-using System.Security.Claims;
 using LibraryBackend.TDO;
 using LibraryBackend.Services;
-using LibraryBackend.Repository;
 using LibraryBackend.Enumerations;
 
 
